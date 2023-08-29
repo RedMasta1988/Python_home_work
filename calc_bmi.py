63302d8a2ad7348f9ad6421f8f0118dfe3535b2c
RedMasta1988 <sankovichev@gmail.com>
2023-08-29 23:10:42 +0300

    home work done

--- calc_bmi.py
+++ calc_bmi.py
@@ -1,7 +1,7 @@
-height, weight = float(input("Рост: ")), float(input("Вес: "))
-BMI = round(weight / ((height / 100) ** 2),1)
-print("Ваш индекс массы тела:", BMI)
-a_MBI, b_MBI = 10, 60
-a1 = round((BMI - a_MBI) / 2) * "="
-b1 = round((b_MBI - BMI) / 2) * "="
-print(str(a_MBI) + a1 + "|" + b1 + str(b_MBI))
+height, weight = int(input("Рост: ")), int(input("Вес: "))
+bmi = round(weight / ((height / 100) ** 2), 1)
+print("Ваш индекс массы тела:", bmi)
+a_bmi, b_bmi = 20, 50
+a1 = round((bmi - a_bmi) / 2) * "="
+b1 = round((b_bmi - bmi) / 2) * "="
+print(str(a_bmi) + a1 + "|" + b1 + str(b_bmi))
