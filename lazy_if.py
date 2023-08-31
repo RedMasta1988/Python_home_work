3ae6e78c911578d98671b26b28ba7b14a9a239b2
RedMasta1988 <136966092+RedMasta1988@users.noreply.github.com>
2023-08-31 21:54:31 +0300

    Update lazy_if.py
    fixed

--- lazy_if.py
+++ lazy_if.py
@@ -9,5 +9,5 @@ if num_1 < num_2 + num_3:
     print(num_2 + num_3 - num_1)
 if num_1 > 50 and (num_2 > num_1 or num_3 > num_1):
     print("Вася")
-if num_1 > 5 or (num_2 == 7 and num_3 == 7):
+if num_1 > 5 or num_2 == 7 and num_3 == 7:
     print("Петя")
