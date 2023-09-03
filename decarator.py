becc2c83f422df31efdf3d6015beca2144022557
RedMasta1988 <sankovichev@gmail.com>
2023-09-03 19:28:57 +0300

    home_work_decarator_done

--- decarator.py
+++ decarator.py
@@ -6,11 +6,11 @@ login = input("Введите login: ")
 def decorator(func):
     @functools.wraps(func)
     def wrapper_decorator(*args, **kwargs):
-        if "admin" in args:
-            print("На Вашем счёте 10 byn")
-        else:
+        if "admin" not in args:
             print("Доступ запрещён")
         value = func(*args, **kwargs)
+        if value == "admin":
+            print("На Вашем счёте 10 byn")
         return value
 
     return wrapper_decorator
