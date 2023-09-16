6ff82fb5e61bec96612d5a82af60a68e50e1cbf7
RedMasta1988 <sankovichev@gmail.com>
2023-09-17 00:00:37 +0300

    flower_create_with_class_and_metod

--- flower_shop/flowers.py
+++ flower_shop/flowers.py
@@ -1,28 +1,29 @@
 class Flower:
-    def __init__(self, name, withering, price):
+    def __init__(self, name=str, withering=int, price=int):
         self.name = name
         self.withering = withering
         self.price = price
 
 
 class Rosa(Flower):
-    def __init__(self, name, withering, price):
-        super().__init__(self, name, withering, price)
+    def __init__(self, lepestki=int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lepestki = lepestki
 
 
 class Pion(Flower):
-    def __init__(self, name, withering, price):
-        super().__init__(self, name, withering, price)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class Tulip(Flower):
-    def __init__(self, name, withering, price):
-        super().__init__(self, name, withering, price)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class Chamomile(Flower):
-    def __init__(self, name, withering, price):
-        super().__init__(self, name, withering, price)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class MyBouquet:
@@ -44,43 +45,43 @@ class MyBouquet:
 
     def bouquet_sorted(self):
         bouquet_sorted = sorted(self.all_flower, key=lambda flower: flower.price)
+        print("Сортировка по цене: ", end=" ")
         for i in bouquet_sorted:
-            print(i.name)
+            print(i.name, end=" ")
+        print()
 
-    def bouquet_search(self,flower_search):
+    def bouquet_search(self, flower_search):
         for i in self.all_flower:
             if flower_search == i.name:
-                print("Цветок:",flower_search,", есть в букете")
-                return
+                print("Цветок:", flower_search, ", есть в букете")
+                break
         else:
-            print("Цветка:",flower_search,", нет в букете")
+            print("Цветка:", flower_search, ", нет в букете")
+
+    def bouquet_info(self):
+        return self.bouquet_price(), self.bouquet_withering()
 
 
-tulip = Flower("Тюльпан", 2, 5)
-rosa = Flower("Роза", 5, 12)
-chamomile = Flower("Ромашка", 8, 3)
-pion = Flower("Пион", 4, 10)
-
-
-all_flower = [tulip, rosa, chamomile, pion]
+tulip = Tulip("Тюльпан", 2, 5)
+rosa = Rosa(25, "Роза", 5, 12)
+chamomile = Chamomile("Ромашка", 8, 3)
+pion = Pion("Пион", 4, 10)
 
 
 bouquet_1 = MyBouquet([tulip, rosa, chamomile, pion])
-bouquet_1.bouquet_price()
-bouquet_1.bouquet_withering()
+bouquet_1.bouquet_info()
 bouquet_1.bouquet_sorted()
 bouquet_1.bouquet_search("Пион")
-
+print("*" * 30)
 
 bouquet_2 = MyBouquet([tulip, rosa, chamomile])
-bouquet_2.bouquet_price()
-bouquet_2.bouquet_withering()
+bouquet_2.bouquet_info()
 bouquet_2.bouquet_sorted()
 bouquet_2.bouquet_search("Пион")
-
+print("*" * 30)
 
 bouquet_3 = MyBouquet([rosa, chamomile])
-bouquet_3.bouquet_price()
-bouquet_3.bouquet_withering()
+bouquet_3.bouquet_info()
 bouquet_3.bouquet_sorted()
 bouquet_3.bouquet_search("Пион")
+print("*" * 30)
